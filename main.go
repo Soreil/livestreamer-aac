86497513d0120f58468e196b738773dbcdc3d6ac
bakape <scorpid33@gmail.com>
2016-03-06 01:00:13 +0200

    Basic 2 to many buffer dispatcher

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"github.com/go-ini/ini"
 	"io/ioutil"
+	"math/rand"
 	"os"
+	"sync"
 )
 
 var config = struct {
@@ -13,6 +15,7 @@ var config = struct {
 
 func main() {
 	readConfig()
+	go fallback()
 }
 
 func throw(err error) {
@@ -39,4 +42,71 @@ func readConfig() {
 	c, err := ini.Load(buf)
 	throw(err)
 	throw(c.MapTo(&config))
+}
+
+// Dispatcher writes the received []byte from either the fallback stream or the
+// transcoded livestreamer output to all connected clients
+type Dispatcher struct {
+	isLive bool
+	sync.Mutex
+	clients  map[int64]chan<- []byte
+	fallback chan []byte
+	stream   chan []byte
+}
+
+// Listen receives messages from
+func (d *Dispatcher) Listen() {
+	for {
+		select {
+		case buf := <-d.fallback:
+			if !d.isLive {
+				d.write(buf)
+			}
+		case buf := <-d.stream:
+			d.write(buf)
+		}
+	}
+}
+
+// Add a client to receive messages from the dispatcher
+func (d *Dispatcher) Add(cl *Client) {
+	d.Lock()
+	for {
+		id := rand.Int63()
+		if _, ok := d.clients[id]; !ok {
+			d.clients[id] = cl.writer
+			cl.id = id
+			break
+		}
+	}
+	d.Unlock()
+}
+
+// Remove removes a client from the set of listening clients
+func (d *Dispatcher) Remove(cl *Client) {
+	d.Lock()
+	delete(d.clients, cl.id)
+	d.Unlock()
+}
+
+func (d *Dispatcher) write(buf []byte) {
+	d.Lock()
+	for _, w := range d.clients {
+		w <- buf
+	}
+	d.Unlock()
+}
+
+// Client is a client connected through HTTP, that is being sent the audio
+// stream.
+type Client struct {
+	id     int64
+	writer chan []byte
+	ip     string
+}
+
+func fallback() {
+	for {
+
+	}
 }
