6be7a3c38250727c4cd69f2633c42f237e8cdece
bakape <scorpid33@gmail.com>
2016-03-10 22:47:29 +0200

    Fallback loopstream

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-
 Checks a specific channel for stream status (ustream only for now) and starts livestreamer. Output is encoded into an ACC stream, which is served through HTTP. Plays a fallback track on loop, when stream is down.
+
+#Dependacies
+* livestreamer
+* ffmpeg compiled with libfdk-aac
+
+#Usage
+* Use `./start.sh` helper script or just run the binary directly
+* Edit `./config.init` to configure
--- main.go
+++ main.go
@@ -3,19 +3,30 @@ package main
 import (
 	"github.com/go-ini/ini"
 	"io/ioutil"
+	"log"
 	"math/rand"
+	"net/http"
 	"os"
+	"os/exec"
 	"sync"
 )
 
 var config = struct {
-	Port, Quality   int
-	Source, Channel string
+	Quality                int
+	Address, URL, Fallback string
 }{}
+
+var dispatcher = Dispatcher{
+	clients:  make(map[int64]chan<- []byte),
+	fallback: make(chan []byte),
+	stream:   make(chan []byte),
+}
 
 func main() {
 	readConfig()
 	go fallback()
+	log.Printf("Listening on %s", config.Address)
+	throw(http.ListenAndServe(config.Address, http.HandlerFunc(serveStream)))
 }
 
 func throw(err error) {
@@ -42,6 +53,36 @@ func readConfig() {
 	c, err := ini.Load(buf)
 	throw(err)
 	throw(c.MapTo(&config))
+}
+
+var headers = map[string]string{
+	"Access-Control-Allow-Origin": "*",
+	"content-type":                "audio/aac",
+	"Expires":                     "Thu, 01 Jan 1970 00:00:00 GMT",
+	"Cache-Control":               "no-cache, no-store",
+	"Transfer-Encoding":           "chunked",
+	"Connection":                  "keep-alive",
+}
+
+func serveStream(res http.ResponseWriter, req *http.Request) {
+	cl := &Client{
+		ip:     req.RemoteAddr,
+		writer: make(chan []byte),
+	}
+	dispatcher.Add(cl)
+
+	head := res.Header()
+	for key, val := range headers {
+		head.Set(key, val)
+	}
+
+	for {
+		_, err := res.Write(<-cl.writer)
+		if err != nil {
+			dispatcher.Remove(cl)
+			break
+		}
+	}
 }
 
 // Dispatcher writes the received []byte from either the fallback stream or the
@@ -79,13 +120,21 @@ func (d *Dispatcher) Add(cl *Client) {
 			break
 		}
 	}
+	log.Printf("%s connected\n", cl.ip)
+	d.printClientCount()
 	d.Unlock()
+}
+
+func (d *Dispatcher) printClientCount() {
+	log.Printf("%v total clients connected\n", len(d.clients))
 }
 
 // Remove removes a client from the set of listening clients
 func (d *Dispatcher) Remove(cl *Client) {
 	d.Lock()
 	delete(d.clients, cl.id)
+	log.Printf("%s disconnected\n", cl.ip)
+	d.printClientCount()
 	d.Unlock()
 }
 
@@ -105,8 +154,23 @@ type Client struct {
 	ip     string
 }
 
+// Play the fallback MP4 in a loop and streams it to the dispatcher
 func fallback() {
 	for {
-
+		ffmpeg := exec.Command(
+			"ffmpeg", "-v", "error",
+			"-re", "-i", config.Fallback,
+			"-c:a", "copy", "-f", "adts", "-bufsize", "496K", "-",
+		)
+		ffmpeg.Stderr = os.Stderr
+		ffmpeg.Stdout = fallbackWriter{}
+		throw(ffmpeg.Run())
 	}
+}
+
+type fallbackWriter struct{}
+
+func (fallbackWriter) Write(buf []byte) (n int, err error) {
+	dispatcher.write(buf)
+	return len(buf), nil
 }
